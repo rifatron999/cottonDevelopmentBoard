827c5407e2d9c16fe202a18ca97b8c0fbb0811e0
Kawsarul Alam Chowdhury Rifat <rifatron999@gmail.com>
2020-07-03 01:26:48 +0600

    employee 07

--- resources/views/administration/employee/allEmployee.blade.php
+++ resources/views/administration/employee/allEmployee.blade.php
@@ -6,6 +6,10 @@
     <div class="container-fluid">
         <div class="row">
             <h1 class="text-center">All Employee</h1>
+            @php
+                use Carbon\Carbon;
+                use Carbon\CarbonInterface;
+            @endphp
             <table class="table table-striped table-hover table-bordered" id="employee_table" width="100%" cellspacing="0" >
                 <thead>
                 <tr>
@@ -16,10 +20,12 @@
                     <th scope="col">NID</th>
                     <th scope="col">Phone</th>
                     <th scope="col">Email</th>
+                    <th scope="col">Retirement</th>
                     <th scope="col">Action</th>
                 </tr>
                 </thead>
                 <tbody>
+
                 @foreach($employees as $key => $s)
                     <tr>
                         <td>{{ $key+1 }}</td>
@@ -38,6 +44,24 @@
                         <td>{{$s->nid_number}}</td>
                         <td>{{$s->phone}}</td>
                         <td>{{$s->email}}</td>
+                        <td>
+                            @php
+                                $retire = $s->retirement_date;
+                                $now = Carbon::now();
+                                $retire_carbon = Carbon::parse($retire);
+                                 if($now->diffInDays($retire_carbon) == 0)
+                            {
+                                echo 'today';
+                            }
+                            else
+                            {
+                                echo $retire_carbon->diffForHumans($now, [
+                                'syntax' => CarbonInterface::DIFF_RELATIVE_TO_NOW,
+                                'options' => Carbon::JUST_NOW | Carbon::ONE_DAY_WORDS
+                            ]);
+                            }
+                            @endphp
+                        </td>
                         <td>
                             <a href="{{route('employeeDetails',Crypt::encrypt($s->id))}}" title="See Details" target="_blank" class="btn btn-primary "><i class="fas fa-arrow-circle-right"></i> </a>
                             <a class="btn btn-success"  href="{{route('employeeEditView',Crypt::encrypt($s->id))}}" target="_blank" title="Edit"><i class="fa fa-edit"></i></a>
